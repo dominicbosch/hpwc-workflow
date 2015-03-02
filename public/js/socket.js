eed8025ab633ff526ec825945a9d2353af24a2d7
Dominic Bosch <dominic.bosch.db@gmail.com>
2015-03-02 15:00:44 +0100

    Changed port and host handling of socket io

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,6 +1,15 @@
-var socket = io.connect( ':443' );
-console.log(socket, 'connectionto :443');
-	socket.on( 'news', function ( data ) {
+
+var port = (location.port === '') ? '' : ':' + location.port;
+var socket = io.connect( port + '/' );
+console.log(socket, 'connectionto ' + port);
+
+socket.on( 'connect', function () {
+	console.log( 'YAY: ');
+});
+socket.on( 'news', function ( data ) {
 	console.log( 'news: ', data );
 	socket.emit( 'my other event', { my: 'data' } );
+});
+socket.on( 'disconnect', function () {
+	console.log( 'DAMMIT: ');
 });
