e7abde2a6eae2c297f5388caa3749619e987ee17
Dominic Bosch <dominic.bosch.db@gmail.com>
2015-04-21 10:46:23 +0200

    move sigint sigterm to head

--- run-hpwc-server.js
+++ run-hpwc-server.js
@@ -1,3 +1,14 @@
+
+process.on( 'SIGINT', function() {
+	console.log( 'Uh-Oh | GOT SIGINT' );
+	process.exit();
+});
+
+process.on( 'SIGTERM', function() {
+	console.log( 'Uh-Oh | GOT SIGTERM' );
+	process.exit();
+});
+
 workflow = require( './index' );
 
 // If you want ssh-keygen stdoutput:
@@ -9,14 +20,4 @@ workflow({
 	// ,
 	// keyfile: 'config/https-key.pem',
 	// certfile: 'config/https-cert.pem'
-});
-
-process.on( 'SIGINT', function() {
-	console.log( 'Uh-Oh | GOT SIGINT' );
-	process.exit();
-});
-
-process.on( 'SIGTERM', function() {
-	console.log( 'Uh-Oh | GOT SIGTERM' );
-	process.exit();
 });
