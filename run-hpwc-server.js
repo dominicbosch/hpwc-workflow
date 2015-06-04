6724fe2869022feae5447280ca4e5e9ca10a45ec
Antonio Maffia <maffia@dmi-sirio.dmi.unibas.ch>
2015-06-04 16:54:33 +0200

    changes that were in branch before the merge

--- run-hpwc-server.js
+++ run-hpwc-server.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 workflow = require( './index' );
 
+process.on( 'SIGINT', function() {
+	console.log( 'Uh-Oh | GOT SIGINT' );
+	process.exit();
+});
+
+process.on( 'SIGTERM', function() {
+	console.log( 'Uh-Oh | GOT SIGTERM' );
+	process.exit();
+});
+
 // If you want ssh-keygen stdoutput:
 // process.env.VERBOSE = true;
 
